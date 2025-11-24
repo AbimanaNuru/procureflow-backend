c4a0242fc86bdb468b6b4d1bf15854eee25e5c71
nuruabimana <nuruabimana@gmail.com>
2025-11-24 18:03:45 +0200

    bugFix: We accept the procureflow-backend.onrender.com in the cros v2

--- p2p_backend/settings.py
+++ p2p_backend/settings.py
@@ -17,7 +17,12 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-produc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com').split(',')
+
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://localhost:3000,http://localhost:5173,https://procureflow-backend.onrender.com'
+).split(',')
 
 
 # Application definition
