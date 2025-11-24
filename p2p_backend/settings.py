4abece81441e8dda00d18ca9f00fd613778ab337
nuruabimana <nuruabimana@gmail.com>
2025-11-24 17:35:04 +0200

    bugFix: We accept the procureflow-backend.onrender.com in the cros

--- p2p_backend/settings.py
+++ p2p_backend/settings.py
@@ -174,7 +174,7 @@ SIMPLE_JWT = {
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://localhost:5173'
+    default='http://localhost:3000,http://localhost:5173,procureflow-backend.onrender.com'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
