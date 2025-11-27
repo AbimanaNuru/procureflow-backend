43f50a920a13e9693e807cd856043cc05c296efb
nuruabimana <nuruabimana@gmail.com>
2025-11-28 00:07:47 +0200

    chore: Update cros by adding the frontend url v2

--- p2p_backend/settings.py
+++ p2p_backend/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 # ----------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 
     # CORS MUST BE ABOVE CommonMiddleware
@@ -137,6 +138,7 @@ USE_TZ = True
 # ----------------------------------------------------
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
