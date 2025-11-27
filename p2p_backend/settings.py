8b6ce271ceb8f12b3d4d8752216f0ec43319770e
nuruabimana <nuruabimana@gmail.com>
2025-11-28 00:15:54 +0200

    chore: Update cros by adding the frontend url v3

--- p2p_backend/settings.py
+++ p2p_backend/settings.py
@@ -189,6 +189,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "http://localhost:3000",
     "http://localhost:5173",
+    "https://procureflow-frontend-1tfs.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
